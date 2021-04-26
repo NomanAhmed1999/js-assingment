012bb0cc3359c96217522a1385d782e34b8d3445
NomanAhmed1999 <81441395+NomanAhmed1999@users.noreply.github.com>
2021-04-27 01:19:15 +0500

    javaScriptAssingment

---
+++ app.js
@@ -0,0 +1,168 @@
+
+
+
+// var arr = "Noman ahmed";
+
+// var a = arr.to();
+// console.log(a)
+
+
+
+
+// var str = "HellO world!";
+// var space = " ";
+
+// // for(var i = 0; i < str.length; i++){
+//     // console.log(i)
+
+//    var j = str.slice(0)
+//    console.log(j)
+// // }
+// // }
+//    if(j === space){
+//        console.log("catched",j)
+//    }else{
+//        console.log("none")
+
+// // //    console.log(j)
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// var userName = prompt('what is your name?');
+// var firstChr = userName[0];
+// var restChr = userName.slice(1);
+// console.log(firstChr.toUpperCase() + restChr.toLowerCase());
+
+
+
+
+
+
+// var statement = "The quick brown fox jumps fox over fox the lazy dog."
+// var foxIndex = statement.indexOf("fox");
+// var foxLastIndex = statement.lastIndexOf("fox");
+
+// var lazyIndex = statement.indexOf("lazy");
+
+// console.log(statement.slice(foxIndex, foxIndex + 3));
+// console.log(statement.slice(lazyIndex, lazyIndex + 4));
+
+
+
+
+
+
+
+
+// var statement = "hello world";
+// for (var i = 0; i < statement.length; i++) {
+//     var foxCase = statement.slice(i, i + 1);
+//     if (foxCase === ' ') {
+//         statement = statement.slice(0, i) + "b" + statement.slice(i + 1);
+//     }
+// }
+// console.log(statement);
+
+// bilal Coding start here ...
+/*
+var statement = "hello world";
+var space = statement.indexOf(" ");
+
+var newthis = statement.charAt(space+2);
+console.log(newthis.toUpperCase() )
+*/
+
+
+
+
+
+
+// var myText = prompt("What's Your Full Name" , "My Full Name is ");
+// console.log("The user enter the text in this format : " + " \n  " + myText);
+
+// var firstChar = myText.slice(0,1);
+// firstChar = firstChar.toUpperCase()
+// // console.log(firstChar);
+
+// var findSpace = myText.indexOf(" ");
+// console.log(findSpace);
+
+// var findSpaceLast = myText.lastIndexOf(" ");
+// console.log(findSpaceLast);
+
+// var lastSpaseChar = myText.charAt(findSpaceLast+1).toUpperCase();
+// console.log(lastSpaseChar)
+
+// var aboveSpacelast = myText.slice(findSpaceLast+2).toLowerCase();
+
+// var spaceChar = myText.charAt(findSpace+1).toUpperCase();
+// // console.log(char)
+
+// var aboveSpace = myText.slice(findSpace+2).toLowerCase();
+// // console.log(aboveSpace)
+
+// var othrChar = myText.slice(1,findSpace).toLowerCase();
+// // console.log(othrChar);
+
+// var capitalze = firstChar + othrChar + " " + spaceChar + aboveSpace;
+// console.log(capitalze);
+
+// **Done**
+
+
+
+
+
+
+
+// var num1 = 3.4;
+
+// console.log(num1);
+
+// num1 = Math.round(num1);
+
+// console.log(num1);
+
+
+
+
+
+
+
+
+//     sir git assingment
+
+// var a = 
+// console.log("value after variable declaration is ", a)
+// a = 3;
+// console.log("initial value",a);
+// a = a + 1;
+// console.log("value is increment",a);
+// console.log("value after adding", a + 6)
+// console.log("value after decrement", a - 1)
+// console.log("value remaining",1)
+
+
+
+
+var ticket = 600;
+console.log("Total cost to buy 5 tickets to buy a movie is ",600*5 + " PKR")
+
+
+
+
+
+
+
+
